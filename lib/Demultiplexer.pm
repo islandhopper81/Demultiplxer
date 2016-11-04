73c408433d0ca0aff767c82c4f00ff837f23e656
Scott Yourstone <scott.yourstone81@gmail.com>
2016-11-04 17:53:55 -0400

    added a test file with mttoolbox seqs. this is also the first functional version.  there are still many things I need to change, update, and improve

--- lib/Demultiplexer.pm
+++ lib/Demultiplexer.pm
@@ -13,6 +13,8 @@ use version; our $VERSION = qv('0.0.1');
 use BioUtils::FastqIO;
 use BioUtils::FastaIO;
 use UtilSY qw(:all);
+use Data::Dumper;
+use Cwd;
 
 # set up the logging environment
 my $logger = get_logger();
@@ -21,27 +23,67 @@ my $logger = get_logger();
 	# Usage statement
 	Readonly my $NEW_USAGE => q{ new( {
 		plate_primer_file => ,
+		fastq_file => ,
+		[output_dir => ,]
 		[metadata_file => ,]
 	})};
+	
+	Readonly my %primers_308 => {
+		"TGA"  => "338F_f1_bc1",
+		"TTGA" => "338F_f2_bc1",
+        "CTTGA"  => "338F_f3_bc1",
+        "ACTTGA"  => "338F_f4_bc1",
+        "GACTTGA" => "338F_f5_bc1",
+        "TGACTTGA"  => "338F_f6_bc1",
+        "ACT"  => "338F_f1_bc2",
+        "TACT" => "338F_f2_bc2",
+        "CTACT"  => "338F_f3_bc2",
+        "ACTACT"  => "338F_f4_bc2",
+        "GACTACT" => "338F_f5_bc2",
+        "TGACTACT"  => "338F_f6_bc2",
+	};
+	
+	Readonly my %primers_806 => {
+		"5" => "806R_f1",
+		"6" => "806R_f2",
+		"7" => "806R_f3",
+		"8" => "806R_f4",
+		"9" => "806R_f5",
+		"10" => "806R_f6",
+	};
 
 	# Attributes #
 	my %plate_primer_file_of;
+	my %plate_to_primers_href_of;
 	my %metadata_file_of;
 	my %index_to_well_file_of;
 	my %index_to_well_href_of;
+	my %fastq_file_of;
+	my %output_dir_of;
 	
 	# Getters #
 	sub get_plate_primer_file;
 	sub get_metadata_file;
 	sub get_index_to_well_href;
 	sub get_index_to_well_file;
+	sub get_well_from_index;
+	sub get_fastq_file;
+	sub get_output_dir;
 
 	# Setters #
 	sub set_plate_primer_file;
 	sub set_index_to_well_href;
+	sub set_fastq_file;
+	sub set_output_dir;
 
 	# Others #
 
+
+	#########
+	# To Do #
+	#########
+	# - print based on metadata file
+	# - what to do with seqs that fail
 
 
 	###############
@@ -57,6 +99,7 @@ my $logger = get_logger();
 		# Make sure the required parameters are defined
 		if ( any {!defined $_}
 				$arg_href->{plate_primer_file},
+				$arg_href->{fastq_file},
 			) {
 			MyX::Generic::Undef::Param->throw(
 				error => 'Undefined parameter value',
@@ -68,7 +111,10 @@ my $logger = get_logger();
 		my $new_obj = bless \do{my $anon_scalar}, $class;
 
 		# Set Attributes
-		$new_obj->set_plate_primer_file($arg_href->{plate_primer_file});
+		$new_obj->set_plate_primer_file($arg_href->{plate_primer_file});		
+		$new_obj->parse_plate_primer_file();
+		$new_obj->set_fastq_file($arg_href->{fastq_file});
+		$new_obj->set_output_dir($arg_href->{output_dir});
 		
 		#if ( defined $arg_href->{metadata_file} ) {
 		#	$new_obj->set_metadata_file($arg_href->{metadata_file});
@@ -97,24 +143,41 @@ my $logger = get_logger();
 		return $metadata_file_of{ident $self};
 	}
 	
-	sub get_well_from_barcode {
-		my ($self, $well) = @_;
+	sub get_well_from_index {
+		my ($self, $index) = @_;
 		
-		is_defined($well);
+		is_defined($index);
 		
-		my $barcode;
-		if ( defined $index_to_well_href_of{ident $self}->{$well} ) {
-			$barcode = $index_to_well_href_of{ident $self}->{$well}
+		my $well;
+		if ( defined $index_to_well_href_of{ident $self}->{$index} ) {
+			$well = $index_to_well_href_of{ident $self}->{$index}
 		}
 		else {
-			
+			#print Dumper($index_to_well_href_of{ident $self});
+			MyX::Generic::Undef->throw(
+				error => "index ($index) not defined in hash"
+			);
 		}
+		
+		return($well)
 	}
 	
 	sub get_index_to_well_file {
 		my ($self) = @_;
 		
 		return $index_to_well_file_of{ident $self};
+	}
+	
+	sub get_fastq_file {
+		my ($self) = @_;
+		
+		return $fastq_file_of{ident $self};
+	}
+	
+	sub get_output_dir {
+		my ($self) = @_;
+		
+		return $output_dir_of{ident $self};
 	}
 
 	###########
@@ -165,6 +228,9 @@ my $logger = get_logger();
 	sub set_index_to_well_href {
 		my ($self, $file) = @_;
 		
+		# note: this overrides any index_to_well inforamtion that is
+		# already set in this object.
+		
 		my $href;
 		
 		# save the file
@@ -181,8 +247,9 @@ my $logger = get_logger();
 			my @vals = ();
 			foreach my $line ( <$IN> ) {
 				chomp($line);
-				next if /^Sample_Well/i;
+				next if ( $line =~ m/^Sample_Well/i );
 				@vals = split("\t", $line);
+				chop $vals[1];
 				$href->{$vals[1]} = $vals[0];
 			}
 			close($IN);
@@ -192,108 +259,434 @@ my $logger = get_logger();
 		
 		return($href);
 	}
+	
+	sub set_fastq_file {
+		my ($self, $file) = @_;
+		
+		is_defined($file);
+		check_file($file);
+		
+		$fastq_file_of{ident $self} = $file;
+		
+		return 1;
+	}
+	
+	sub set_output_dir {
+		my ($self, $dir) = @_;
+		
+		if ( ! defined $dir ) {
+			$dir = getcwd();
+		}
+		
+		$output_dir_of{ident $self} = $dir;
+		
+		return 1;
+	}
 
 	##########
 	# Others #
 	##########
+	sub split_fastq {
+		my ($self) = @_;
+		
+		# get the primer info
+		#my $primers_to_frame = $self->parse_plate_primer_file();
+		
+		# variables
+		my $total = 0;
+		my $count = 0;
+		my $header;
+		my $fwd_fs;  # forward frame shift
+		my $rev_fs;  # reverse frame shift
+		my $index;
+		my $fwd_query;
+		my $rev_query;
+		my $match_fwd;
+		my $match_rev;
+		my $plate;
+		my $well;
+		my $count_id;  # the seq count id in the original seq header ie P0_(13134)
+		
+		# open the fastq file
+		my $in = BioUtils::FastqIO->new( {stream_type => '<',
+										  file => $self->get_fastq_file()} );
+
+		# Read in all the sequences
+		my %seqs = ();  #hash with KEY=>sampleID VALUE=>aref of seqs
+		while ( my $seq = $in->get_next_seq() ) {
+			$header = $seq->get_header();
+			
+			eval {
+				$fwd_fs = _get_fwd_fs($header);
+				$rev_fs = _get_rev_fs($header);
+				$index = _get_index($header);
+				$count_id = _get_count_id($header);
+				
+				$fwd_query = _get_fwd_query($fwd_fs);
+				$rev_query = length $rev_fs;
+			
+				# find the fwd match
+				$match_fwd = $self->_get_fwd_match($fwd_query);
+				
+				# find the rev match
+				$match_rev = $self->_get_rev_match($rev_query);
+				
+				# get the plate and well for the read
+				$plate = $self->_get_plate($match_fwd, $match_rev);
+				$well = $self->get_well_from_index($index);
+				
+				_update_seq_id($seq, $plate, $well, $count_id);
+				
+				_add_seq($seq->to_FastaSeq(), \%seqs, $plate . $well);
+			};
+			# skip this sequence if any of the regex's fail
+			if ( my $e = Exception::Class->caught('MyX::Generic::UnmatchedRegex') ) {
+				# non matching regex -- throw an error
+				print $e->error, "\n";
+				next;
+			}
+			elsif ( $e = Exception::Class->caught('MyX::Generic') ) {
+				# any of the other MyX::Generic errors
+				print $e->error, "\n";
+				next;
+			}
+			elsif ( $@ ) {
+				print $@, "\n";
+			}
+		}
+		
+		# output all the seqs to their files
+		$self->_output_seqs(\%seqs);
+	}
+	
+	sub _update_seq_id {
+		my ($seq, $plate, $well, $count_id) = @_;
+		
+		my $header = $seq->get_header();
+		my $new_id = $plate . $well . "_" . $count_id;
+		my $new_header;
+		
+		if ( $header =~ m/P\d+_\d+ (.*)/ ) {
+			$new_header = $new_id . " " . $1;
+		}
+		else {
+			print "ERROR\n";
+		}
+		
+		$seq->set_header($new_header);
+		
+		return 1;
+	}
+	
+	sub _output_seqs {
+		my ($self, $seqs_href) = @_;
+		
+		# create a dir in the output dir called samples
+		my $root_dir = $self->get_output_dir();
+		my $dir = "$root_dir/samples";
+		mkdir $dir;
+		
+		foreach my $sample ( keys %{$seqs_href} ) {
+			my $file = "$dir/$sample\.fasta";
+			my $fasta_out = BioUtils::FastaIO->new({stream_type => '>',
+													file => $file});
+			
+			foreach my $seq ( @{$seqs_href->{$sample}} ) {
+				$fasta_out->write_seq($seq);
+			}
+		}
+	}
+	
+	sub _add_seq {
+		my ($seq, $seq_href, $sample_id) = @_;
+		
+		if ( exists $seq_href->{$sample_id} ) {
+			push @{$seq_href->{$sample_id}}, $seq;
+		}
+		else {
+			my @arr = ();
+			push @arr, $seq;
+			$seq_href->{$sample_id} = \@arr;
+		}
+		
+		return 1;
+	}
+	
+	sub _get_plate {
+		my ($self, $fwd_match, $rev_match) = @_;
+		
+		my $href = $plate_to_primers_href_of{ident $self};
+		my $plate_found;
+		foreach my $plate ( keys %{$href} ) {
+			if ( $href->{$plate}{$fwd_match} and
+				 $href->{$plate}{$rev_match} ) {
+				$plate_found = $plate;
+			}
+		}
+		
+		if ( ! defined $plate_found ) {
+			# throw some error
+			MyX::Generic->throw(
+				error => "Cannot find the plate from fwd_match " .
+						 "($fwd_match) and rev_match ($rev_match)"
+			);
+		}
+		
+		return($plate_found);
+	}
+	
+	sub _get_fwd_match {
+		my ($self, $fwd_query) = @_;
+		
+		my $match_fwd;
+		if ( defined $primers_308{$fwd_query} ) {
+			$match_fwd = $primers_308{$fwd_query};
+		}
+		else {
+			# non matching fwd primer -- throw an error
+			MyX::Generic->throw(
+				error => "fwd query not found ($fwd_query)"
+			);
+		}
+		
+		return($match_fwd);
+	}
+	
+	sub _get_rev_match {
+		my ($self, $rev_query) = @_;
+		
+		my $match_rev;
+		if ( defined $primers_806{$rev_query} ) {
+			$match_rev = $primers_806{$rev_query};
+		}
+		else {
+			# non matching rev primer -- throw an error
+			MyX::Generic->throw(
+				error => "rev query not found ($rev_query)"
+			);
+		}
+		
+		return($match_rev);
+	}
+	
+	sub _get_fwd_query {
+		my ($fwd_fs) = @_;
+		
+		my $fwd_query;
+		if ( $fwd_fs =~ m/^[A-Z]{4}([A-Z]+)[A-Z]{4}$/ ) {
+			$fwd_query = $1;
+		}
+		else {
+			MyX::Generic::UnmatchedRegex->throw(
+				error => "Cannot find fwd query in $fwd_fs"
+			);
+		}
+		
+		return( $fwd_query );
+	}
+	
+	sub _get_fwd_fs {
+		my ($header) = @_;
+		
+		my $fwd_fs;
+		if ( $header =~ m/\S+ (\S+)-\S+/ ) {
+			$fwd_fs = $1;
+		}
+		else {
+			MyX::Generic::UnmatchedRegex->throw(
+				error => "Cannot find fwd frameshift barcode in $header"
+			);
+		}
+		
+		return($fwd_fs);
+	}
+	
+	sub _get_rev_fs {
+		my ($header) = @_;
+		
+		my $rev_fs;
+		if ( $header =~ m/\S+ \S+-(\S+)/ ) {
+			$rev_fs = $1;
+		}
+		else {
+			MyX::Generic::UnmatchedRegex->throw(
+				error => "Cannot find rev frameshift barcode in $header"
+			);
+		}
+		
+		return($rev_fs);
+	}
+	
+	sub _get_index {
+		my ($header) = @_;
+		
+		my $index;
+		if ( $header =~ m/\S+ \S+ \S+ \d+:\w+:\d+:(\w+)/ ) {
+			$index = $1;
+		}
+		else {
+			MyX::Generic::UnmatchedRegex->throw(
+				error => "Cannot find index in $header"
+			);
+		}
+		
+		# remove the last base
+		chop($index);
+		
+		return($index);
+	}
+	
+	sub _get_count_id {
+		my ($header) = @_;
+		
+		my $count_id;
+		if ( $header =~ m/\S+_(\d+) \S+ \S+ \S+/ ) {
+			$count_id = $1;
+		}
+		else {
+			MyX::Generic::UnmatchedRegex->throw(
+				error => "Cannot find count id in $header"
+			);
+		}
+		
+		return($count_id);
+	}
+	
+	sub parse_plate_primer_file {
+		my ($self) = @_;
+		
+		# returns a hash with ??
+		
+		my $file = $self->get_plate_primer_file();
+		open my $IN, "<", $file or
+			MyX::Generic::File::CannotOpen->throw(
+				error => "Cannot open file ($file)"
+			);
+		
+		my @vals = ();
+		my @fwd_frames = ();
+		my @rev_frames = ();
+		my %plate_to_frames = ();
+		foreach my $line ( <$IN> ) {
+			chomp $line;
+			@vals = split("\t", $line);
+			@fwd_frames = split(",", $vals[1]);
+			@rev_frames = split(",", $vals[2]);
+			
+			foreach my $fwd_frame ( @fwd_frames ) {
+				$plate_to_frames{$vals[0]}{$fwd_frame}++;
+			}
+			foreach my $rev_frame ( @rev_frames ) {
+				$plate_to_frames{$vals[0]}{$rev_frame}++;
+			}
+		}
+		close($IN);
+		
+		#print Dumper(%plate_to_frames);
+		
+		$plate_to_primers_href_of{ident $self} = \%plate_to_frames;
+		
+		return(\%plate_to_frames);
+	}
+	
 	sub _get_default_index_href {
 		my $href = {
-			"A1" => "CGTCGGTA",
-			"B1" => "GTGTCCAA",
-			"C1" => "TCCATGCG",
-			"D1" => "AGGTTCGC",
-			"E1" => "GTCGAAGC",
-			"F1" => "ACGGCTGA",
-			"G1" => "CTATCTGG",
-			"H1" => "TGGACTCT",
-			"A2" => "ATTGTGAG",
-			"B2" => "TGTCGTCA",
-			"C2" => "CGTTCTAA",
-			"D2" => "TGTGAACC",
-			"E2" => "GGCCTATC",
-			"F2" => "GGATATAG",
-			"G2" => "GCTGAAGA",
-			"H2" => "CGGTGTCT",
-			"A3" => "GTCAGCTG",
-			"B3" => "GCCGACTT",
-			"C3" => "CTAAGGAG",
-			"D3" => "TCAGGCCA",
-			"E3" => "TACTTGCA",
-			"F3" => "CTTACTAG",
-			"G3" => "TGATCCTA",
-			"H3" => "ACGTTCAT",
-			"A4" => "TCGCACAA",
-			"B4" => "ACCATCGT",
-			"C4" => "AAGGCACG",
-			"D4" => "GCTAGTTC",
-			"E4" => "CGCTGAAT",
-			"F4" => "CACCGATT",
-			"G4" => "CATGGACG",
-			"H4" => "CATCTTAC",
-			"A5" => "CTGTAACA",
-			"B5" => "GATGATCG",
-			"C5" => "AGCCGTTA",
-			"D5" => "TAAGCATG",
-			"E5" => "TGTGCGTA",
-			"F5" => "TGGCTCTA",
-			"G5" => "CGACCTTA",
-			"H5" => "AGTGCCAC",
-			"A6" => "CACTTCTG",
-			"B6" => "CGCTAGTA",
-			"C6" => "GGAACGCT",
-			"D6" => "ATGACTCA",
-			"E6" => "GTAGGACC",
-			"F6" => "GTACGCGT",
-			"G6" => "AGATGGCT",
-			"H6" => "ATACGGAC",
-			"A7" => "GAACGTAT",
-			"B7" => "AACCAGCT",
-			"C7" => "GGCGCTTA",
-			"D7" => "CTTCGCAG",
-			"E7" => "CGGCTACA",
-			"F7" => "CTCTACAG",
-			"G7" => "GCTTAATA",
-			"H7" => "ACCTCAGA",
-			"A8" => "CGAATCCT",
-			"B8" => "TAGCAGTG",
-			"C8" => "GTGACATG",
-			"D8" => "CGGCAGAA",
-			"E8" => "CAGCGTGT",
-			"F8" => "GTGTATGC",
-			"G8" => "ACATTGCG",
-			"H8" => "TCTTCGAG",
-			"A9" => "GCAACGTC",
-			"B9" => "GCAGCTCT",
-			"C9" => "GAGGTTAC",
-			"D9" => "CATGAAGT",
-			"E9" => "AGTATGCA",
-			"F9" => "GATGCCTT",
-			"G9" => "GCGAATAC",
-			"H9" => "ATCTGCGA",
-			"A10" => "AACAGGTG",
-			"B10" => "TACCATGA",
-			"C10" => "CCAACTAG",
-			"D10" => "ATAGTCCG",
-			"E10" => "TTAAGCGA",
-			"F10" => "ACGTCCTG",
-			"G10" => "CAGTAATG",
-			"H10" => "TTCCATAG",
-			"A11" => "TGCACAAT",
-			"B11" => "CATGAGGC",
-			"C11" => "ACAGGAGT",
-			"D11" => "AGCCTTCT",
-			"E11" => "TGACTAGT",
-			"F11" => "CGAGTATC",
-			"G11" => "CAATGTCG",
-			"H11" => "GCGTCACG",
-			"A12" => "TTATAGGC",
-			"B12" => "GAGTGCTA",
-			"C12" => "ACGTCTTA",
-			"D12" => "TTGTGCAC",
-			"E12" => "GGCGTTAC",
-			"F12" => "GTCTCGCA",
-			"G12" => "TCTTGACG",
-			"H12" => "GTGCTACT"
+			"A1" => "CGTCGGT",
+			"B1" => "GTGTCCA",
+			"C1" => "TCCATGC",
+			"D1" => "AGGTTCG",
+			"E1" => "GTCGAAG",
+			"F1" => "ACGGCTG",
+			"G1" => "CTATCTG",
+			"H1" => "TGGACTC",
+			"A2" => "ATTGTGA",
+			"B2" => "TGTCGTC",
+			"C2" => "CGTTCTA",
+			"D2" => "TGTGAAC",
+			"E2" => "GGCCTAT",
+			"F2" => "GGATATA",
+			"G2" => "GCTGAAG",
+			"H2" => "CGGTGTC",
+			"A3" => "GTCAGCT",
+			"B3" => "GCCGACT",
+			"C3" => "CTAAGGA",
+			"D3" => "TCAGGCC",
+			"E3" => "TACTTGC",
+			"F3" => "CTTACTA",
+			"G3" => "TGATCCT",
+			"H3" => "ACGTTCA",
+			"A4" => "TCGCACA",
+			"B4" => "ACCATCG",
+			"C4" => "AAGGCAC",
+			"D4" => "GCTAGTT",
+			"E4" => "CGCTGAA",
+			"F4" => "CACCGAT",
+			"G4" => "CATGGAC",
+			"H4" => "CATCTTA",
+			"A5" => "CTGTAAC",
+			"B5" => "GATGATC",
+			"C5" => "AGCCGTT",
+			"D5" => "TAAGCAT",
+			"E5" => "TGTGCGT",
+			"F5" => "TGGCTCT",
+			"G5" => "CGACCTT",
+			"H5" => "AGTGCCA",
+			"A6" => "CACTTCT",
+			"B6" => "CGCTAGT",
+			"C6" => "GGAACGC",
+			"D6" => "ATGACTC",
+			"E6" => "GTAGGAC",
+			"F6" => "GTACGCG",
+			"G6" => "AGATGGC",
+			"H6" => "ATACGGA",
+			"A7" => "GAACGTA",
+			"B7" => "AACCAGC",
+			"C7" => "GGCGCTT",
+			"D7" => "CTTCGCA",
+			"E7" => "CGGCTAC",
+			"F7" => "CTCTACA",
+			"G7" => "GCTTAAT",
+			"H7" => "ACCTCAG",
+			"A8" => "CGAATCC",
+			"B8" => "TAGCAGT",
+			"C8" => "GTGACAT",
+			"D8" => "CGGCAGA",
+			"E8" => "CAGCGTG",
+			"F8" => "GTGTATG",
+			"G8" => "ACATTGC",
+			"H8" => "TCTTCGA",
+			"A9" => "GCAACGT",
+			"B9" => "GCAGCTC",
+			"C9" => "GAGGTTA",
+			"D9" => "CATGAAG",
+			"E9" => "AGTATGC",
+			"F9" => "GATGCCT",
+			"G9" => "GCGAATA",
+			"H9" => "ATCTGCG",
+			"A10" => "AACAGGT",
+			"B10" => "TACCATG",
+			"C10" => "CCAACTA",
+			"D10" => "ATAGTCC",
+			"E10" => "TTAAGCG",
+			"F10" => "ACGTCCT",
+			"G10" => "CAGTAAT",
+			"H10" => "TTCCATA",
+			"A11" => "TGCACAA",
+			"B11" => "CATGAGG",
+			"C11" => "ACAGGAG",
+			"D11" => "AGCCTTC",
+			"E11" => "TGACTAG",
+			"F11" => "CGAGTAT",
+			"G11" => "CAATGTC",
+			"H11" => "GCGTCAC",
+			"A12" => "TTATAGG",
+			"B12" => "GAGTGCT",
+			"C12" => "ACGTCTT",
+			"D12" => "TTGTGCA",
+			"E12" => "GGCGTTA",
+			"F12" => "GTCTCGC",
+			"G12" => "TCTTGAC",
+			"H12" => "GTGCTAC"
 		};
 		
 		# I want the barcode as the key
@@ -384,6 +777,11 @@ Log::Log4perl qw(:easy)
 Log::Log4perl::CommandLine qw(:all)
 MyX::Generic
 version; our $VERSION = qv('0.0.1')
+BioUtils::FastqIO
+BioUtils::FastaIO
+UtilSY qw(:all)
+Data::Dumper
+Cwd
 
 
 =head1 INCOMPATIBILITIES
@@ -410,6 +808,9 @@ None reported.
 	set_plate_primer_file
 	get_metadata_file
 	set_metadata_file
+	get_index_to_well_file
+	set_index_to_well_file
+	get_well_from_index
 
 =back
 
@@ -420,12 +821,17 @@ None reported.
 	Title: new
 	Usage: Demultiplexer->new({
 				plate_primer_file => $file,
+				fastq_file => $fastq_file,
+				[output_dir => ,]
+				[metadata_file => ,]
 			});
 	Function:
 	Returns: Demultiplexer
 	Args: -plate_primer_file => links plates to frameshifted primers
+	      -fastq_file => Fastq file output from MT-Toolbox
 	Throws: MyX::Generic::Undef::Param
-	Comments: NA
+	Comments: If no output_dir is provided the current working directory
+	          is used.
 	See Also: NA
 	
 =head2 get_plate_primer_file
@@ -474,6 +880,100 @@ None reported.
 	        MyX::Generic::DoesNotExist::File
 	        MyX::Generic::File::Empty
 	Comments: NA
+	See Also: NA
+	
+=head2 get_index_to_well_file
+
+	Title: get_index_to_well_file
+	Usage: $obj->get_index_to_well_file()
+	Function: Returns path to optional index to well mapping file
+	Returns: str
+	Args: NA
+	Throws: NA
+	Comments: OPTIONAL
+	See Also: NA
+	
+=head2 set_index_to_well_file
+
+	Title: set_index_to_well_file
+	Usage: $obj->set_index_to_well_file($file)
+	Function: sets the path to the index to well mapping file
+	Returns: 1 on success
+	Args: -file => Path to file
+	Throws: MyX::Generic::Undef::Param
+	        MyX::Generic::DoesNotExist::File
+	        MyX::Generic::File::Empty
+	Comments: NA
+	See Also: NA
+	
+=head2 get_well_from_index
+
+	Title: get_well_from_index
+	Usage: $obj->get_well_from_index($index)
+	Function: Returns the well matching the given indes
+	Returns: str
+	Args: -index => illumina barcode (ie index)
+	Throws: MyX::Generic::Undef::Param
+	        MyX::Generic::Undef
+	Comments: NA
+	See Also: NA
+	
+=head2 parse_plate_primer_file
+	
+	Tilte: parse_plate_primer_file
+	Usage: $obj->parse_plate_primer_file()
+	Function: Parse the plate primer file and return that info in a hash ref
+	Returns: Href
+	Args: NA
+	Throws: MyX::Generic::File::CannotOpen
+	Comments: NA
+	See Also: NA
+	
+=head2 get_fastq_file
+
+	Title: get_fastq_file
+	Usage: $obj->get_fastq_file()
+	Function: Returns path to FASTQ file output from MT-Toolbox
+	Returns: str
+	Args: NA
+	Throws: NA
+	Comments: NA
+	See Also: NA
+	
+=head2 set_fastq_file
+
+	Title: set_fastq_file
+	Usage: $obj->set_fastq_file($file)
+	Function: sets the path to the FASTQ file output from MT-Toolbox
+	Returns: 1 on success
+	Args: -file => Path to FASTQ file
+	Throws: MyX::Generic::Undef::Param
+	        MyX::Generic::DoesNotExist::File
+	        MyX::Generic::File::Empty
+	Comments: NA
+	See Also: NA
+	
+=head2 get_output_dir
+
+	Title: get_output_dir
+	Usage: $obj->get_output_dir()
+	Function: Returns path to the output directory
+	Returns: str
+	Args: NA
+	Throws: NA
+	Comments: NA
+	See Also: NA
+	
+=head2 set_output_dir
+
+	Title: set_output_dir
+	Usage: $obj->set_output_dir($dir)
+	Function: Sets the path to the output directory
+	Returns: 1 on success
+	Args: -dir => Path to output directory
+	Throws: NA
+	Comments: If no output_dir parameter is provided in the new function
+	          the defualt output_dir is the current working directory.
 	See Also: NA
 
 
